9f165f901f53a7496d0f23b99a2d407a5eaeca48
FredrickMburu <mburu.fredrick21@students.dkut.ac.ke>
2024-06-08 11:29:56 +0300

    recipe app using react using spoonacular api

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Home from "./pages/Home"
+import Pages from "./pages/Pages"
+
+function App() {
+
+
+  return (
+    <>
+      <h1>Hello</h1>
+      <Pages />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Veggie from '../components/Veggie.jsx'
+import Popular from '../components/Popular.jsx'
+
+function Home() {
+  return (
+    <div>
+      <Veggie />
+      <Popular />
+    </div>
+  )
+}
+
+export default Home
