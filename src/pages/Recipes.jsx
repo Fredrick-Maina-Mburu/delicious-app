fc70904b19120d002403ecd4b1bfc174423d208c
FredrickMburu <mburu.fredrick21@students.dkut.ac.ke>
2024-06-09 02:03:24 +0300

    done with the instruction and ingredients page

--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import styled from 'styled-components'
 import { useParams } from 'react-router-dom'
-import { useState } from 'react';
+import { useState, useEffect} from 'react';
 
 function Recipes() {
   const params = useParams();
   const [details, setDetails] = useState({});
+  const [activeTab, setActiveTab] = useState("instructions");
 
   const fetchDetails = async () => {
 
-    await fetch(`https://api.spoonacular.com/recipes/${params.name}/information?apiKey=${import.meta.env.VITE_API_KEY}`);
+   const data = await fetch(`https://api.spoonacular.com/recipes/${params.name}/information?apiKey=${import.meta.env.VITE_API_KEY}`);
 
     const detailData = await data.json();
     setDetails(detailData);
@@ -26,8 +27,23 @@ function Recipes() {
         <img src={details.image} alt="" />
       </div>
       <Info>
-        <Button>Instructions</Button>
-        <Button>Ingredients</Button>
+        <Button className={activeTab === 'instructions' ? 'active' : ''} onClick={() => setActiveTab("instructions")}>Instructions</Button>
+        <Button className={activeTab === 'ingredients' ? 'active' : ''} onClick={() => setActiveTab("ingredients")}>Ingredients</Button>
+        {activeTab === "instructions" && (
+          <div>
+          <h3 dangerouslySetInnerHTML={{ __html: details.summary}}></h3>
+          <h3 dangerouslySetInnerHTML={{ __html: details.instructions}}></h3>
+        </div>
+        )}
+        {activeTab === "ingredients" && (
+          <ul>
+          {details.extendedIngredients.map((ingredient) => {
+            return(
+              <li key={ingredient.id}>{ingredient.original}</li>
+            )
+          })}
+        </ul>
+        )}
       </Info>
     </DetailWrapper>
   )
@@ -53,13 +69,13 @@ const DetailWrapper = styled.div`
   }
 `;
 
-const Button = styled.div`
+const Button = styled.button`
   padding: 1rem 2rem;
   color: #313131;
   background: white;
   border: 2px solid black;
   margin-right: 2rem;
-  font-weight: 600
+  font-weight: 600;
 `;
 
 const Info = styled.div`
